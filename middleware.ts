2e0a0359ca7799f5303f1e842327fe03c3fb71ac
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-02-25 10:51:08 +0000

    middleware and authorisation

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { auth } from "./auth";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
     const userLoggedIn = !!req.auth;
     console.log("Route: ", req.nextUrl.pathname);
-    console.log("Route: ", userLoggedIn);
+    console.log("Route (Is Logged in): ", userLoggedIn);
 });
 
 // Optionally, don't invoke Middleware on some paths
