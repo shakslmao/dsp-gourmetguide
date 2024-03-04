4601ad64f77f581fcdee32a5d987b152fc4feb8d
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-03-04 12:13:39 +0000

    debugging

--- components/initial-preferences/initial-michelin-prefs.tsx
+++ components/initial-preferences/initial-michelin-prefs.tsx
@@ -11,13 +11,12 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { MichelinStarValidationSchema, TMichelinStarOptionValidationSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
 
 export const InitialMichelinPrefs = () => {
-    const { preferences } = useUserPreferences();
+    const { preferences, updatePreferences } = useUserPreferences();
     const router = useRouter();
-    const handleNextOnClick = () => {
-        router.push("");
-    };
+
     const handlePrevOnClick = () => {
         router.push("/inital-preferences/radiuspreferences");
     };
@@ -25,12 +24,20 @@ export const InitialMichelinPrefs = () => {
         resolver: zodResolver(MichelinStarValidationSchema),
     });
 
-    const onSubmit = (data: TMichelinStarOptionValidationSchema) => {
-        toast({
-            title: "Preferences Updated",
-            description: "Your preferences have been updated successfully.",
-        });
-    };
+    const onSubmit = useCallback(
+        (data: TMichelinStarOptionValidationSchema) => {
+            const prefersMichelinRated = data.type == "yes" ? true : false;
+            updatePreferences({ ...preferences, prefersMichelinRated });
+
+            toast({
+                title: "Preferences Updated",
+                description: "Your preferences have been updated successfully.",
+            });
+
+            router.push("/");
+        },
+        [preferences, updatePreferences, router]
+    );
 
     console.log(preferences);
 
@@ -56,33 +63,21 @@ export const InitialMichelinPrefs = () => {
                                                 onValueChange={field.onChange}
                                                 defaultValue={field.value}
                                                 className="flex justify-center space-x-10">
-                                                {" "}
-                                                {/* Ensures horizontal layout and spacing */}
                                                 <FormItem className="flex items-center">
-                                                    {" "}
-                                                    {/* Flex layout for alignment */}
-                                                    <FormControl className="flex items-center">
-                                                        {" "}
-                                                        {/* Ensure the FormControl also aligns items in center */}
-                                                        <RadioGroupItem value="all" />
-                                                        <FormLabel className="ml-2">
-                                                            Yes
-                                                        </FormLabel>{" "}
-                                                        {/* Margin left for spacing */}
+                                                    <FormControl>
+                                                        <RadioGroupItem value="yes" />
                                                     </FormControl>
+                                                    <FormLabel className="ml-2 text-center mb-4">
+                                                        Yes
+                                                    </FormLabel>
                                                 </FormItem>
                                                 <FormItem className="flex items-center">
-                                                    {" "}
-                                                    {/* Flex layout for alignment */}
-                                                    <FormControl className="flex items-center">
-                                                        {" "}
-                                                        {/* Ensure the FormControl also aligns items in center */}
-                                                        <RadioGroupItem value="none" />
-                                                        <FormLabel className="ml-2">
-                                                            No
-                                                        </FormLabel>{" "}
-                                                        {/* Margin left for spacing */}
+                                                    <FormControl>
+                                                        <RadioGroupItem value="no" />
                                                     </FormControl>
+                                                    <FormLabel className="ml-2 text-center">
+                                                        No
+                                                    </FormLabel>
                                                 </FormItem>
                                             </RadioGroup>
                                         </FormControl>
@@ -91,34 +86,34 @@ export const InitialMichelinPrefs = () => {
                             />
                         </div>
                     </form>
+
+                    <p className="text-xs text-center font-light">
+                        Please choose if you would like to see restaurants that have been awarded
+                        <span className="text-green-600"> michelin</span> stars, this will help us
+                        match you with the restaurants that fit your preferences.
+                    </p>
+
+                    <div className="flex justify-evenly gap-x-4">
+                        <Button
+                            onClick={() => handlePrevOnClick()}
+                            className={buttonVariants({
+                                variant: "default",
+                                className: "w-full py-2 text-white bg-black rounded-lg",
+                                size: "sm",
+                            })}>
+                            Previous
+                        </Button>
+                        <Button
+                            type="submit"
+                            className={buttonVariants({
+                                variant: "default",
+                                className: "w-full py-2 text-white bg-black rounded-lg",
+                                size: "sm",
+                            })}>
+                            Next
+                        </Button>
+                    </div>
                 </Form>
-
-                <p className="text-xs text-center font-light">
-                    Please choose if you would like to see restaurants that have been awarded
-                    <span className="text-green-600"> michelin</span> stars, this will help us match
-                    you with the restaurants that fit your preferences.
-                </p>
-
-                <div className="flex justify-evenly gap-x-4">
-                    <Button
-                        onClick={() => handlePrevOnClick()}
-                        className={buttonVariants({
-                            variant: "default",
-                            className: "w-full py-2 text-white bg-black rounded-lg",
-                            size: "sm",
-                        })}>
-                        Previous
-                    </Button>
-                    <Button
-                        onClick={() => handleNextOnClick()}
-                        className={buttonVariants({
-                            variant: "default",
-                            className: "w-full py-2 text-white bg-black rounded-lg",
-                            size: "sm",
-                        })}>
-                        Next
-                    </Button>
-                </div>
             </div>
         </div>
     );
